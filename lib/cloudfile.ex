452c023612f20864d73b9d1dfe0ee76cc0aff004
William Carroll <wpcarro@gmail.com>
2017-04-21 11:50:21 -0400

    initial commit

---
+++ lib/cloudfile.ex
@@ -0,0 +1,18 @@
+defmodule Cloudfile do
+  @moduledoc """
+  Documentation for Cloudfile.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cloudfile.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cloudfile_test.exs
@@ -0,0 +1,8 @@
+defmodule CloudfileTest do
+  use ExUnit.Case
+  doctest Cloudfile
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
